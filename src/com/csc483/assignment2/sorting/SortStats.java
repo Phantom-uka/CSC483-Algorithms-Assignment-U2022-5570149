d0f743a3dc39e5c05e20981175fdbcb83a14764b
Uka-Victory <ukavictory20@gmail.com>
2026-04-05 16:41:37 +0100

    Add SortStats class for sorting statistics
    Introduced a class to track sorting algorithm statistics.

---
+++ src/com/csc483/assignment2/sorting/SortStats.java
@@ -0,0 +1,9 @@
+package com.csc483.assignment2.sorting;
+
+/**
+ * Stores statistics for sorting algorithms.
+ */
+public class SortStats {
+    public long comparisons = 0;
+    public long swaps = 0;
+}
